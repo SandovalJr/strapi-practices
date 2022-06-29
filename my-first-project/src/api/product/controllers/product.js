f70108cef704b823850165367ec23e2eacfaa09c
SandovalJr <edubizzle7@gmail.com>
2022-06-29 11:33:04 -0500

    ya agrega producto

--- my-first-project/src/api/product/controllers/product.js
+++ my-first-project/src/api/product/controllers/product.js
@@ -6,4 +6,34 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController('api::product.product',
+    ({ strapi }) => ({
+
+        // CREAR PRODUCT 1
+        // async create(ctx, next) {
+        //     const response = await super.create(ctx);
+        //     return response
+        // }
+
+        async create(ctx, next) {
+            //  crearemos la orden (sin confirmar aun)
+            const producto = await strapi.entityService.create("api::product.product", {
+                data: {
+                    // relaciones
+                    name: ctx.request.body.data.name,
+                    product_code: ctx.request.body.data.product_code,
+                }
+            })
+            console.log(producto);
+
+            return { producto }
+        }
+
+
+        
+    })
+
+
+
+
+);
